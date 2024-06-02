4828cd690fc214c1c646c3db865ff041fb7f9f4c
LAILATUL CARISMA P. <carismaputri903@gmail.com>
2024-06-02 09:12:04 +0700

    tambah baru

---
+++ src/scripts/views/pages/pariwisata.js
@@ -0,0 +1,13 @@
+const Pariwisata = {
+  async render() {
+    return `
+      <h2>Pariwisata</h2>
+    `;
+  },
+ 
+  async afterRender() {
+    // Fungsi ini akan dipanggil setelah render()
+  },
+};
+ 
+export default Pariwisata;
