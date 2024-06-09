60f78536b3224226b30c4a86946603bdf2d24acc
unknown <syarifyahya.pmk@gmail.com>
2024-06-09 20:02:55 +0700

    add package-lock json

--- src/scripts/views/templates/template-creator.js
+++ src/scripts/views/templates/template-creator.js
@@ -57,6 +57,9 @@ const createTourDetailTemplate = (data) => `
     <p><strong>Province:</strong> ${data.province}</p>
     <p><strong>Address:</strong> ${data.address}</p>
   </div>
+  <div class="card-body d-flex flex-column">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d37544.92212712898!2d119.8566777499065!3d-8.458101902097969!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2db468a6d47ed641%3A0x87f524333c6a6e8d!2sLabuan%20Bajo%2C%20Komodo%2C%20West%20Manggarai%20Regency%2C%20East%20Nusa%20Tenggara%2C%20Indonesia!5e0!3m2!1sen!2skr!4v1717904033036!5m2!1sen!2skr" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  </div>
 </div>
 `;
 
