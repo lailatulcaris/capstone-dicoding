74a7567ca46df21ffb0457f28902b2c6d30feac6
unknown <syarifyahya.pmk@gmail.com>
2024-06-15 11:58:02 +0700

    meningkatkan web performance (masi remed jir)

--- src/scripts/components/app-footer.js
+++ src/scripts/components/app-footer.js
@@ -7,11 +7,7 @@ class AppFooter extends HTMLElement {
     this.innerHTML = `
     <div class="container d-flex justify-content-between align-items-center">
         <div class="text-center d-flex align-items-center">
-          <picture>
-            <source type="image/webp" srcset="./logo-tanpa-bg.webp" class="logo-img mr-2 lazyload">
-            <source type="image/jpeg" srcset="./logo-tanpa-bg.jpg" class="logo-img mr-2 lazyload">
             <img data-src="./logo-tanpa-bg.png" alt="Logo" class="logo-img mr-2 lazyload" />
-          </picture>
           
           <p class="mb-0">&copy; 2024, Travel in.</p>
         </div>
